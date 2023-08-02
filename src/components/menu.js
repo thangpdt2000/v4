7dd3510fc9fd7807591ebb86ed8ace120932bce0
thangpdt2000 <samson@vmodev.com>
2023-08-02 22:16:39 +0700

    update

--- src/components/menu.js
+++ src/components/menu.js
@@ -266,7 +266,7 @@ const Menu = () => {
               </ol>
             )}
 
-            <a href="/resume_phamducthang.pdf" className="resume-link">
+            <a href="/resume.pdf" className="resume-link">
               Resume
             </a>
           </nav>
--- src/components/nav.js
+++ src/components/nav.js
@@ -218,11 +218,7 @@ const Nav = ({ isHome }) => {
   );
 
   const ResumeLink = (
-    <a
-      className="resume-button"
-      href="/resume_phamducthang.pdf"
-      target="_blank"
-      rel="noopener noreferrer">
+    <a className="resume-button" href="/resume.pdf" target="_blank" rel="noopener noreferrer">
       Resume
     </a>
   );
